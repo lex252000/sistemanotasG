b92af9abf7648e674faae27694dae09c109cb7ff
Carlos22819 <carlos.abad.hurtado@utelvt.edu.ec>
2023-08-28 21:36:07 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=sistemanotas',
     'username' => 'root',
-    'password' => '',
+    'password' => 'reydereyes',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
